59b8935bbdaee3b06e957c313356effe319010b3
Jose Nino <jdninom@unal.edu.co>
2023-07-23 17:34:45 -0700

    Update .gitignore

--- __init__.py
+++
@@ -1,8 +0,0 @@
-"""
-CaliDatos
-====================================
-
-CaliDatos is a complete package that let's you perform quality checks on
-your data based on a data dictionary that contains metadata for your fields.
-
-"""
